4662fe13165ceface6fcdcb7e6d39661e439ad8d
onetk <info.media.tk@gmail.com>
2019-05-23 19:23:11 +0900

    FizzBuzz

---
+++ FizzBuzz.go
@@ -0,0 +1,22 @@
+package main
+
+import(
+	"fmt"
+)
+
+func main(){
+
+	for i:=0; i<100;i++{
+		switch{
+		case i%15==0:
+			fmt.Println("FizzBuzz")
+		case i%5==0:
+			fmt.Println("Buzz")
+		case i%3==0:
+			fmt.Println("Fizz")
+		default:
+			fmt.Println(i)
+		}
+	}
+
+}
