06ad2625dd1bbf5a160976e079aa04da8c50128e
onetk <info.media.tk@gmail.com>
2019-05-23 20:54:51 +0900

    Fibonacci

--- FizzBuzz.go
+++ FizzBuzz.go
@@ -1,18 +1,18 @@
 package main
 
-import(
+import (
 	"fmt"
 )
 
-func main(){
+func main() {
 
-	for i:=0; i<100;i++{
-		switch{
-		case i%15==0:
+	for i := 0; i < 100; i++ {
+		switch {
+		case i%15 == 0:
 			fmt.Println("FizzBuzz")
-		case i%5==0:
+		case i%5 == 0:
 			fmt.Println("Buzz")
-		case i%3==0:
+		case i%3 == 0:
 			fmt.Println("Fizz")
 		default:
 			fmt.Println(i)
